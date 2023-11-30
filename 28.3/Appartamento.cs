397d99e0f6fd9843cb22abc16d5b822d77278a6f
R1cky <R1cky@PC-RICKY>
2023-11-30 21:58:22 +0100

    Aggiungere i file di progetto.

---
+++ 28.3/Appartamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._3
+{
+    internal class Appartamento : Immobile
+    {
+        public int nLocali { get; set; }
+        public int nBagni { get; set; }
+    }
+}
---
+++ 28.3/Immobile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._3
+{
+    internal class Immobile
+    {
+        public string Codice { get; set; }
+        public string Indirizzo { get; set; }
+        public int CAP { get; set; }
+        public string Città { get; set; }
+        public double Superficie { get; set; }
+
+        public List<Box> Box { get; set; }
+        public List<Appartamento> Appartamenti { get; set; }
+        public List<Villa> Ville { get; set; }
+
+    }
+}
